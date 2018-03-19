66b4ed9fbf40b3dfa2c6d2e2c607473aaea2e922
github username <github email address>
2018-03-19 12:29:08 -0400

    using pseudocode to better go through the challenge. I will try incorporating it on my next challenge

--- solution.js
+++ solution.js
@@ -1,24 +1,34 @@
-//Only does it for one row...
+function checksum(nestedArr) {
+  //keep track of each row with a sum variable
+  let sum = 0
+
+  //get through each element/row of the nested array
+  for (const row of nestedArr) {
+    //sort it from least to greatest
+    // let sortedRow = row.sort((a,b) => (a - b))
+    let sortedRow = row.sort(function compare(a,b) {
+      return a - b
+    })
+    console.log(sortedRow)
+
+    //find difference then add to sum
+    diff = Math.abs(sortedRow[0] - sortedRow[sortedRow.length - 1])
+    sum = sum + diff
+    console.log(sum)
+  }
+
+  //return the sum
+  return sum
+}
 
 // function checksum(nestedArr) {
 //   let sum = 0
-//   for (const row of nestedArr) {
-//     let sortedRows = row.sort(function compare(a,b){
+//   for (i = 0; i < nestedArr.length; i++) {
+//     let sortedRows = nestedArr[i].sort(function compare(a,b){
 //       a - b
 //     })
-//     sum = sortedRows[0] - sortedRows[sortedRows.length - 1]
-//     return sum
+//     let rowSum = sortedRows[0] - sortedRows[sortedRows.length - 1]
+//     let sum = sum + rowSum
 //   }
+//   return Math.abs(sum)
 // }
-
-function checksum(nestedArr) {
-  let sum = 0
-  for (i = 0; i < nestedArr.length; i++) {
-    let sortedRows = nestedArr[i].sort(function compare(a,b){
-      a - b
-    })
-    rowSum = sortedRows[0] - sortedRows[sortedRows.length - 1]
-    sum = sum + rowSum
-  }
-  return Math.abs(sum)
-}
