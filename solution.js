3cf489120cbe71557434df740953a5f4bc649e2f
cs-j <cj@mail.co.de>
2018-03-19 09:52:15 -0400

    Done

---
+++ solution.js
@@ -0,0 +1,22 @@
+function checkSum(array) {
+  let array = []
+
+  for (let i = 0; i < array.length; i++) {
+    let arrayMax = Math.max(...array[i])
+    let arrayMin = Math.min(...array[i])
+    let diff = max - min
+    diffs.push(diff)
+  }
+
+  function sortedArray(a, b) {
+    return a + b
+  }
+
+  // const lastNum = sortedArray[array.length - 1]
+  // const firstNum = sortedArray[0]
+
+  function sum() {
+    const sum = lastNum - firstNum
+  }
+  return sum
+}
